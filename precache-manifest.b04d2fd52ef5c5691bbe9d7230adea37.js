aa8bbfc36683cf612902b3febf40d0e2521ad669
Huzaifa Habib <huzaifa.dj@gmail.com>
2020-03-29 23:32:26 +0500

    Updates

---
+++ precache-manifest.b04d2fd52ef5c5691bbe9d7230adea37.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61dc526a8695e44799e502ac6a6186fb",
+    "url": "/react-coursera/index.html"
+  },
+  {
+    "revision": "fcf7a5a006aca61c109e",
+    "url": "/react-coursera/static/css/2.f391ba32.chunk.css"
+  },
+  {
+    "revision": "86f2e8c7303ba40667ee",
+    "url": "/react-coursera/static/css/main.37cffc5e.chunk.css"
+  },
+  {
+    "revision": "fcf7a5a006aca61c109e",
+    "url": "/react-coursera/static/js/2.409b9564.chunk.js"
+  },
+  {
+    "revision": "86f2e8c7303ba40667ee",
+    "url": "/react-coursera/static/js/main.87a70db2.chunk.js"
+  },
+  {
+    "revision": "d69ab5d57e16b388e025",
+    "url": "/react-coursera/static/js/runtime~main.d5497d76.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-coursera/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-coursera/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-coursera/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-coursera/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-coursera/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
